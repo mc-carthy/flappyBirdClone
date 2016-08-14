f4cddf2a2d0a33f88b68a5270455fcd144387fac
Mick McCarthy <mccarthy.mick0012@gmail.com>
2016-08-14 15:53:12 +0100

    Create pipe pair prefab

---
+++ Assets/Scripts/movePipes.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class movePipes : MonoBehaviour {
+
+	public Vector2 velocity = new Vector2(-4, 0);
+
+	private Rigidbody2D rb;
+
+	private void Start () {
+		rb = GetComponent<Rigidbody2D> ();
+		rb.velocity = velocity;
+	}
+}
