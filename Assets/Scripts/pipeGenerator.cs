343a3d11f6f6cfb148be547af607a21975aa3986
Mick McCarthy <mccarthy.mick0012@gmail.com>
2016-08-14 16:10:16 +0100

    Add pipeGenerator script to randomly generate pipes

---
+++ Assets/Scripts/pipeGenerator.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class pipeGenerator : MonoBehaviour {
+
+	public GameObject pipePair;
+	public float minY, maxY;
+
+	private void Start () {
+		InvokeRepeating ("CreatePipes", 1f, 3.5f);
+	}
+
+	private void CreatePipes () {
+		Instantiate (pipePair, new Vector3(transform.position.x, Random.Range(minY, maxY), 0f), Quaternion.identity);
+	}
+}
