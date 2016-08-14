fbfc2e5bab2296889df2acd92d72869cf667568c
Mick McCarthy <mccarthy.mick0012@gmail.com>
2016-08-14 15:39:33 +0100

    Add player, background and player controller

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public Vector2 jumpForce = new Vector2(0, 300);
+
+	private Rigidbody2D rb;
+
+	private void Start () {
+		rb = GetComponent<Rigidbody2D> ();
+	}
+
+	private void Update () {
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			rb.velocity = Vector2.zero;
+			rb.AddForce (jumpForce);
+		}
+	}
+}
